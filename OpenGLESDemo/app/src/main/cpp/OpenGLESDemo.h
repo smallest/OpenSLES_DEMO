24386bdd54bc66038613dd90409e90fcb35335bd
smallest <xuzhudeng@gmail.com>
2018-08-23 09:24:53 +0800

    OpenSLESDemo

--- OpenGLESDemo/app/src/main/cpp/OpenGLESDemo.h
+++ OpenGLESDemo/app/src/main/cpp/OpenGLESDemo.h
@@ -1,11 +1,39 @@
-#ifndef OPENGLESDEMO_OPENGLESDEMO_H
-#define OPENGLESDEMO_OPENGLESDEMO_H
+#ifndef OPENGLESDEMO_OPENGLESPlYAER_H
+#define OPENGLESDEMO_OPENGLESPlYAER_H
+
 #include <android/log.h>
-#include <SLES/OpenSLES.h>
+
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "smallest", __VA_ARGS__)
-class OpenGLESDemo {
-    static SLresult init();
-    static void playTest();
-    static void destory();
+
+class OpenGLESPlayer {
+public:
+    OpenGLESPlayer();
+    void release();
+    void prepare();
+    void start();
+    void setDataSource(File* pcmFile);
+private:
+    int32_t createEngine();
+    int32_t createOutputMix();
+    int32_t setDataSource();
+    int32_t setDataSink();
+private:
+    SLObjectItf engineObject;
+    SLEngineItf engineEngine;
+
+    SLObjectItf outputMixObject;
+    SLEnvironmentalReverbItf reverb;
+    SLEnvironmentalReverbSettings reverbSettings = SL_I3DL2_ENVIRONMENT_PRESET_STONECORRIDOR;
+
+    SLObjectItf playerObject;
+    SLPlayItf playerPlay;
+    SLVolumeItf playerVolume;
+
+    SLAndroidSimpleBufferQueueItf simpleBufferQueue;
+    FILE *pcmFile;
+    void *buffer;
+    uint8_t *out_buffer;
+
+    
 };
-#endif //OPENGLESDEMO_OPENGLESDEMO_H
+#endif //OPENGLESDEMO_OPENGLESPlYAER_H
--- OpenGLESDemo/app/src/main/cpp/OpenSLESDemo.cpp
+++ OpenGLESDemo/app/src/main/cpp/OpenSLESDemo.cpp
@@ -2,8 +2,19 @@
 #include <SLES/OpenSLES.h>
 #include <SLES/OpenSLES_Android.h>
 
-SLresult OpenGLESDemo::init() {
-    SLObjectItf engineObject;
+OpenGLESPlayer::OpenGLESPlayer():
+    engineObject(NULL),
+    engineEngine(NULL),
+    outputMixObject(NULL),
+    reverb(NULL),
+    playerObject(NULL),
+    playerPlay(NULL),
+    playerVolume(NULL) {
+    createEngine();
+}
+
+int32_t OpenGLESPlayer::createEngine() {
+    LOGD("createEngine()"); 
     SLresult result = slCreateEngine(&engineObject, 0, NULL, 0, NULL, NULL);
     if(result != SL_RESULT_SUCCESS) {
         LOGD("slCreateEngine failed, result=%d", result);
@@ -14,13 +25,39 @@ SLresult OpenGLESDemo::init() {
         LOGD("engineObject Realize failed, result=%d", result);
         return result;
     }
-    SLEngineItf engineEngine;
     result = (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, &(engineEngine));
     if(result != SL_RESULT_SUCCESS) {
         LOGD("engineObject GetInterface failed, result=%d", result);
         return result;
     }
     return result;
+}
+
+
+int32_t OpenGLESPlayer::createOutputMix() {
+    const SLInterfaceID mids[1] = {SL_IID_ENVIRONMENTALREVERB};
+    const SLboolean mreq[1] = {SL_BOOLEAN_FALSE};
+    SLresult result = (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, 1, mids, mreq);
+    if(result != SL_RESULT_SUCCESS) {
+        LOGD("CreateOutputMix failed, result=%d", result);
+        return result;
+    }
+    result = (*outputMixObject)->Realize(outputMixObject, SL_BOOLEAN_FALSE);
+    if(result != SL_RESULT_SUCCESS) {
+        LOGD("Realize failed, result=%d", result);
+        return result;
+    }
+    result = (*outputMixObject)->GetInterface(outputMixObject, SL_IID_ENVIRONMENTALREVERB, &outputMixEnvironmentalReverb);
+    if(result != SL_RESULT_SUCCESS) {
+        LOGD("GetInterface failed, result=%d", result);
+        return result;
+    }
+    result = (*outputMixEnvironmentalReverb)->SetEnvironmentalReverbProperties(outputMixEnvironmentalReverb, &reverbSettings);
+    if(result != SL_RESULT_SUCCESS) {
+        LOGD("SetEnvironmentalReverbProperties failed, result=%d", result);
+        return result;
+    }
+    SLDataLocator_OutputMix outputMix = {SL_DATALOCATOR_OUTPUTMIX, outputMixObject};
 }
 
 void OpenGLESDemo::playTest() {
